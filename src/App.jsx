b30962d553aa18db216f991df067f1e13d74b33d
Affaled <120531449+Affaled@users.noreply.github.com>
2023-04-18 12:11:41 -0300

    Criação Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Outlet />
     </div>
   );
 }
