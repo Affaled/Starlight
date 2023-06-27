46428a7efe11294af83e62a222b86f927fd488e1
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-27 19:10:59 -0300

    configurações iniciais

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import "../assets/styles/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
