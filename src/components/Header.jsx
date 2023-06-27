232f16207b678af877ca4dee18dfc54b77e02e44
Affaled <120531449+Affaled@users.noreply.github.com>
2023-06-27 19:11:32 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import Star from "../assets/icons/star.svg";
+import "../assets/styles/Header.css";
+import "../assets/styles/Header-navbar.css";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="Header_container">
+      <div className="Logo_container">
+        <img src={Star} alt="logo" />
+        <h1>Starlight</h1>
+      </div>
+      <nav className="Navbar_container">
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
+          <li>
+            <a href="#">GALERY</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
