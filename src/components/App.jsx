2feea07c74eef23d4ee63c0eae8afd5de4d90163
Affaled <120531449+Affaled@users.noreply.github.com>
2023-07-20 18:33:30 -0300

    recomeço

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./Header";
-import Home from "../pages/Home";
 import "../assets/styles/App.css";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
     </div>
   );
 }
