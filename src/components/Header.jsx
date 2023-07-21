92512df053f26346e04d9a730ee7fc2fb150255a
Affaled <120531449+Affaled@users.noreply.github.com>
2023-07-21 17:50:04 -0300

    criação do conteúdo da header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import icon from "../assets/svg/header_icon.svg";
 
 export default function Header() {
-  return <header>text</header>;
+  return (
+    <header>
+      <div>
+        <img src={icon} alt="logo icon" />
+        <div>
+          <h1>STARLIGHT</h1>
+          <h2>GEEK PARTY</h2>
+        </div>
+      </div>
+      <nav>
+        <ul>
+          <li>INÍCIO</li>
+          <li>SOBRE</li>
+          <li>GALERIA</li>
+          <li>
+            <button>COMPRAR</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
